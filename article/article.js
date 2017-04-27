f9a66bf170be2daa3360de8b12f32ad5febd1d85
xll032 <376013043@qq.com>
2017-04-27 17:18:02 +0800

    23

---
+++ article/article.js
@@ -0,0 +1,42 @@
+var len = 40;
+var app = angular.module('article',[]);
+app.controller('aCtrl',function($scope,$http){
+    function getId(){
+        var href = location.href;
+        return (href.substring(href.indexOf('?')+4))>>0;
+    }
+    function switchType(type){
+        switch(type){
+            case 1:return 'jQuery';
+            case 2:return 'AngularJS';
+            case 4:return 'React';
+            case 8:return 'Vue';
+            case 16:return '原生JS';
+            case 32:return 'CSS';
+            case 64:return 'HTML';
+            case 128:return '微信小程序';
+            case 256:return '插件';
+            case 512:return '其他';
+        }
+    }
+    $scope.id = getId();
+    $scope.up = (($scope.id-1)>0)?'article.html?id='+($scope.id-1):'javascript:void(0)';
+    $scope.down = (($scope.id)<len)?'article.html?id='+($scope.id+1):'javascript:void(0)';
+    $scope.upText = (($scope.id-1)>0)?'上一篇':'首篇';
+    $scope.downText = (($scope.id)<len)?'下一篇':'末篇';
+    var promise = $http({
+        url:'../public/articleData/article'+$scope.id+'.json',
+        method:'get'
+    });
+    promise.then(function(res){
+        var _data = res.data;
+        $scope.content = _data.content;
+        $scope.title = _data.title;
+        $scope.time = _data.time;
+        $scope.type = switchType(_data.type);
+        document.getElementsByTagName('article')[0].innerHTML = $scope.content;
+    });
+    promise.catch(function(res){
+        console.error(res);
+    });
+});
